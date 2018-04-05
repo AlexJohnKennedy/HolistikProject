0007ba2de27d99bd731adac53d7b28e247254732
AlexJohnKennedy <alexkfridges@gmail.com>
2018-04-05 10:36:15 +1000

    connected interact.js library with test canvas to figure out how to make it work, at the moment, nodes are draggable and are restricted to the canvas area

--- public/javascripts/canvasController.js
+++ public/javascripts/canvasController.js
@@ -27,6 +27,47 @@ let currIdNum = 0;
 const idPrefix  = "contentNode";
 
 
+//TEST TEST TEST -- See if i can access interact.js functions...
+interact('.draggable').draggable({
+    inertia : true,     //Enable inertia for the draggable elements
+    restrict: {
+        restriction: "parent",  //Keep the moveable object within the boundaries of it's HTML parent element
+        endOnly: true,
+        elementRect: {top: 0, left: 0, bottom: 1, right: 1}
+    },
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+        let textEl = event.target.querySelector('p');
+
+        textEl && (textEl.textContent =
+            'moved a distance of '
+            + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+            Math.pow(event.pageY - event.y0, 2) | 0))
+                .toFixed(2) + 'px');
+    }
+});
+
+function dragMoveListener (event) {
+    let target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+}
+
+// this is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
+
 
 // ---------------------------------------------------------------------------------------------------------------------
 // --- Node creation and deletion functionality ------------------------------------------------------------------------
