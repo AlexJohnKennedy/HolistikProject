432fff64ed0a1ff338b35145e3ba174a0c54c42d
Jaiden Fairoze <jkfairoze@gmail.com>
2018-04-17 14:24:55 +1000

    changing some colours

--- public/javascripts/canvasController.js
+++ public/javascripts/canvasController.js
@@ -18,7 +18,7 @@ const defaultNodePosition = {
     x : 100,     //Corresponding values to CSS 'absolute translation' coordinates.
     y : 100
 };
-const defaultColour = "dodgerblue";
+const defaultColour = "rgb(194, 184, 176)";
 const defaultNodeSize = {
     height : 50,
     width  : 100
