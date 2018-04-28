1c07bfd35c5f03a9e1ffadee38ca6399502415e2
AlexJohnKennedy <alexkfridges@gmail.com>
2018-04-28 10:38:10 +1000

    started implementing full rebuild request class

--- public/javascripts/clientAjaxRequests.js
+++ public/javascripts/clientAjaxRequests.js
@@ -8,6 +8,18 @@
  * directory).
  */
 
+//Constant api routes that we will use. MUST MATCH THE SERVER-SPECIFIED ROUTES! (See 'routes' folder in the project dir)
+const PROJECT_STRUCTURE_LOAD_URL   = "/loadProjectStructure";
+const PROJECT_ARRANGEMENT_LOAD_URL = "/loadProjectArrangement";
+const LOAD_ARRANGEMENT_URL         = "/loadArrangement";
+
+const PROJECT_STRUCTURE_SAVE_URL   = "/saveProjectStructure";
+const PROJECT_ARRANGEMENT_SAVE_URL = "/saveProjectArrangement";
+const SAVE_ARRANGEMENT_URL         = "/saveArrangement";
+
+// ---------------------------------------------------------------------------------------------------------------------
+// --- Http AJAX request wrapper object --------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 
 /**
  * Simple object constructor to create a wrapper object which handles making GET and POST XMLHttpRequests for AJAX
@@ -54,4 +66,60 @@ function HttpClientWrapper() {
         //Send the request!
         request.send(bodyString);   //Send the bodyString in the POST message body.
     };
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// --- AJAX Request for saving and loading nodes -----------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Define a ES6 'Class'. When the application page is loaded, it will be associated with a given project.
+ * On loading, the server will pass in the database id of the project. The browser window will store that id and use it to
+ * send project-specific AJAX requests to save and load node content when required.
+ *
+ * This syntactic-sugar-'class' specifies an object prototype which will encapsulate AJAX request functionality for a
+ * given project.
+ */
+class ajaxProjectLoader {
+
+    //Constructor. All we need to pass in to any instantiated project loader object is the project id which we will use
+    //to tell the server which project we are loading/saving to/from
+    constructor(projectId) {
+        this.projectId = projectId;
+
+        //Instantiate a http client wrapper object to help us send async AJAX requests.
+        this.httpClient = new HttpClientWrapper();
+    }
+
+    // --- Methods --------------------------------------------------------------------
+
+    /**
+     * Makes simple 'load structure' and 'load arrangement' requests and then re-builds the project from scratch when
+     * the server returns the resulting JSON.
+     */
+    loadProjectFromServer() {
+        let structureJSON = null;
+        let arrangementJSON = null;
+        let pendingRequestCount = 2;
+
+        this.httpClient.sendJsonPostRequest(PROJECT_STRUCTURE_LOAD_URL, this.projectId, function(response) {
+            structureJSON = response;
+            pendingRequestCount--;
+
+            if (pendingRequestCount <= 0) {
+                fullyRebuildCanvasStateFromJSON(structureJSON, arrangementJSON, contextNodeId);
+            }
+        });
+
+        this.httpClient.sendJsonPostRequest(PROJECT_ARRANGEMENT_LOAD_URL, this.projectId, function(response) {
+            arrangementJSON = response;
+            pendingRequestCount--;
+
+            if (pendingRequestCount <= 0) {
+                fullyRebuildCanvasStateFromJSON(structureJSON, arrangementJSON, contextNodeId);
+            }
+        });
+
+    }
+
 }
