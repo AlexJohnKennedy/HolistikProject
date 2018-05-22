bce53763e14c8c62e2b02d647b4f26b50f9c5e5e
AlexJohnKennedy <alexkfridges@gmail.com>
2018-05-22 20:14:48 +1000

    added highlight formatting option

--- public/javascripts/documentPageScripts/dynamicFormattingOptions.js
+++ public/javascripts/documentPageScripts/dynamicFormattingOptions.js
@@ -82,3 +82,60 @@ function toggleShowBorderlines() {
     //Toggle the state for next time around.
     showingBorders = !showingBorders;
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+let showingHighlights = true;
+
+function toggleHighlighting() {
+    //Okay. For all the matching elements in the document, simply set their display to none!
+    let docElem = document.getElementById("mainDocument");
+
+    //Get all the relationship tag elements, by their identifying class!
+    let tags = docElem.getElementsByClassName("nodeContent");
+
+    if (showingHighlights) {
+        removeHighlightListeners();
+    }
+    else {
+        addHighlightListeners();
+    }
+
+    showingHighlights = !showingHighlights;
+}
+
+function addHighlightListeners() {
+    //Acquire the link objects
+    let docElem = document.getElementById("mainDocument");
+    let links = docElem.getElementsByClassName("nodeLink");
+
+    for (let link of links) {
+        link.addEventListener("mouseenter", highlightCallback);
+        link.addEventListener("mouseleave", unhighlightCallback);
+    }
+}
+
+function removeHighlightListeners() {
+    //Acquire the link objects
+    let docElem = document.getElementById("mainDocument");
+    let links = docElem.getElementsByClassName("nodeLink");
+
+    for (let link of links) {
+        link.removeEventListener("mouseenter", highlightCallback);
+        link.removeEventListener("mouseleave", unhighlightCallback);
+    }
+}
+
+//Callbacks
+function highlightCallback(event) {
+    let targetId   = event.currentTarget.getAttribute("href").substring(1);  //Get href target and trim the '#' colour.
+    let targetElem = document.getElementById(targetId);
+
+    targetElem.classList.add("highlight");
+}
+function unhighlightCallback(event) {
+    let targetId   = event.currentTarget.getAttribute("href").substring(1);  //Get href target and trim the '#' colour.
+    let targetElem = document.getElementById(targetId);
+
+    targetElem.classList.remove("highlight");
+}
